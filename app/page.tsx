885f9bbf09585fc8082985206b65fe56349b1d65
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-18 13:44:57 +0530

    feat: Assets and MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <p className="text-white p-2">Hi</p>
+      <p className="h1-bold p-2">Hi</p>
+      <p className="h1-bold p-2 font-space-grotesk">Hi</p>
     </div>
   );
 }
