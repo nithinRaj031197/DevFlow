4f24e82b1c71a0d95204f93ac5b9c63a40879559
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-18 19:11:15 +0530

    feat: Theme Context, ShadCn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <p className="h1-bold p-2">Hi</p>
-      <p className="h1-bold p-2 font-space-grotesk">Hi</p>
-    </div>
-  );
+  return <div className="flex justify-center items-center min-h-screen"></div>;
 }
