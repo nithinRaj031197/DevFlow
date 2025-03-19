4572946895288386972eeb1549e9a1e97a0e783f
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-19 10:09:37 +0530

    AuthForm

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-screen items-center justify-center .light-bg bg-cover bg-center bg-no-repeat px-4 py-10 dark:dark-bg">
+    <main className="flex min-h-screen items-center justify-center bg-[url('/images/auth-light.png')] bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-[url('/images/auth-dark.png')]">
       <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
         <div className="flex items-center justify-between gap-2">
           <div className="space-y-2.5">
