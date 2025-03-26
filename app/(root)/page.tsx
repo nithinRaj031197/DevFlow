efb28a181cea38cba77eb1552c069b302b6e4306
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-26 12:27:39 +0530

    Implement Question Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,23 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
 import React from "react";
 
-const questions = [
+const questions: Question[] = [
   {
     _id: "1",
     title: "How to learn React?",
-    description: "I want to learn React, can anyone help me?",
+    content: "I want to learn React, can anyone help me?",
     tags: [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
     ],
-    author: { _id: "1", name: "John Doe" },
+    author: { _id: "1", name: "John Doe", image: "" },
     upvotes: 10,
+    downvotes: 2,
     answers: 5,
     views: 100,
     createdAt: new Date(),
@@ -22,13 +25,14 @@ const questions = [
   {
     _id: "2",
     title: "What is the difference between var, let, and const?",
-    description: "I am confused about when to use var, let, or const in JavaScript.",
+    content: "I am confused about when to use var, let, or const in JavaScript.",
     tags: [
       { _id: "2", name: "JavaScript" },
       { _id: "3", name: "Variables" },
     ],
-    author: { _id: "2", name: "Jane Smith" },
+    author: { _id: "2", name: "Jane Smith", image: "" },
     upvotes: 25,
+    downvotes: 3,
     answers: 8,
     views: 230,
     createdAt: new Date(),
@@ -36,13 +40,14 @@ const questions = [
   {
     _id: "3",
     title: "Best practices for using useEffect in React?",
-    description: "I often get confused with dependencies in useEffect. What are some best practices?",
+    content: "I often get confused with dependencies in useEffect. What are some best practices?",
     tags: [
       { _id: "1", name: "React" },
       { _id: "4", name: "Hooks" },
     ],
-    author: { _id: "3", name: "Alice Johnson" },
+    author: { _id: "3", name: "Alice Johnson", image: "" },
     upvotes: 18,
+    downvotes: 1,
     answers: 3,
     views: 145,
     createdAt: new Date(),
@@ -55,7 +60,6 @@ interface SearchParams {
 
 const Home = async ({ searchParams }: SearchParams) => {
   const { query = "" } = await searchParams;
-  console.log(query);
 
   const filteredQuestions = questions?.filter((question) => question.title.toLowerCase().includes(query?.toLowerCase()));
   return (
@@ -66,13 +70,16 @@ const Home = async ({ searchParams }: SearchParams) => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
+
       <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch route="/" imgSrc="/icons/search.svg" placeholder="Search questions..." otherClasses="flex-1" />
       </section>
-      Home Filter
+
+      <HomeFilter />
+
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
-          <p>{question.title}</p>
+          <QuestionCard key={question._id} question={question} />
         ))}
       </div>
     </>
---
+++ types/global.d.ts
@@ -0,0 +1,83 @@
+interface Tag {
+  _id: string;
+  name: string;
+  questions?: number;
+}
+
+interface Author {
+  _id: string;
+  name: string;
+  image: string;
+}
+
+interface Question {
+  _id: string;
+  title: string;
+  content: string;
+  tags: Tag[];
+  author: Author;
+  createdAt: Date;
+  upvotes: number;
+  downvotes: number;
+  answers: number;
+  views: number;
+  createdAt: Date;
+}
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+interface RouteParams {
+  params: Promise<Record<string, string>>;
+  searchParams: Promise<Record<string, string>>;
+}
+
+interface PaginatedSearchParams {
+  page?: number;
+  pageSize?: number;
+  query?: string;
+  filter?: string;
+  sort?: string;
+}
+
+interface Answer {
+  _id: string;
+  author: Author;
+  content: string;
+  createdAt: Date;
+  upvotes: number;
+  downvotes: number;
+  question: string;
+}
+
+interface Collection {
+  _id: string;
+  author: string | Author;
+  question: Question;
+}
+
+interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+  createdAt: Date;
+}
+
+interface BadgeCounts {
+  GOLD: number;
+  SILVER: number;
+  BRONZE: number;
+}
