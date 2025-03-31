761e58199eb08898f9226bcfed7f6fef4f4480dd
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-31 16:02:22 +0530

    Implement Create and Get routes for users

--- database/user.model.ts
+++ database/user.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models } from "mongoose";
+import { model, models, Schema, Document } from "mongoose";
 
 export interface IUser {
   name: string;
@@ -11,22 +11,21 @@ export interface IUser {
   reputation?: number;
 }
 
-const UserSchema = new Schema(
+export interface IUserDoc extends IUser, Document {}
+const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     bio: { type: String },
-    image: { type: String, required: true },
+    image: { type: String },
     location: { type: String },
     portfolio: { type: String },
-    reputation: { type: String, default: 0 },
+    reputation: { type: Number, default: 0 },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-const User = models.user || model<IUser>("User", UserSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
 
 export default User;
--- types/global.d.ts
+++ types/global.d.ts
@@ -34,6 +34,12 @@ type ActionResponse<T = null> = {
   status?: number;
 };
 
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type APIErrorResponse = NextResponse<ErrorResponse>;
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
+
 interface RouteParams {
   params: Promise<Record<string, string>>;
   searchParams: Promise<Record<string, string>>;
