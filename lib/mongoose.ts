d3736de69f0496b86b6292811f9a163cb0a13162
nithinRaj031197 <nithinraj.official3@gmail.com>
2025-03-31 15:38:41 +0530

    Error Handler

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,8 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+import "@/database";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
@@ -24,6 +27,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing mongoose connection");
     return cached.conn;
   }
 
@@ -33,9 +37,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "devflow",
       })
       .then((result) => {
+        logger.info("Connected to MongoDB");
         return result;
       })
       .catch((error) => {
+        logger.error("Error connecting to MongoDB", error);
         throw error;
       });
   }
